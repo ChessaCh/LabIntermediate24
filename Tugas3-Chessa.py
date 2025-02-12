0649fbb88d880815bc7ab33f74d86bc1f9d5772f
Chessa Chandra Devi <chessachandrad@gmail.com>
2025-02-12 21:26:57 +0700

    push

--- Tugas3-Chessa.py
+++ Tugas3-Chessa.py
@@ -1,28 +1,28 @@
 
 def hangman(secret_word):
-    guess_word=['_ ' *len(secret_word)]
+    kata=['_ ' *len(secret_word)]
     attempt=6
     data=[]
-    while guess_word!=list(secret_word) and attempt>0: 
-        print(' '.join(guess_word))
-        guess = input("Guess the secret word (put a single letter): ").lower()
+    while kata!=list(secret_word) and attempt>0: 
+        print(' '.join(kata))
+        tebak= input("Guess the secret word (put a single letter): ").lower()
 
-        if len(guess)!=1:
+        if len(tebak)!=1:
             print("Please enter only ONE letter.")
             continue
-        elif not guess.isalpha():
+        elif not tebak.isalpha():
             print("You only can guess an APLHABET")
         
-        if guess in secret_word:
+        if tebak in secret_word:
             print("This letter is available in the secret word.")
 
             for index, letter in enumerate(secret_word):
-                if letter == guess:
-                    guess_word[index]=guess
+                if letter == tebak:
+                    kata[index]=tebak
                     attempt-=1
                     print("You used 1 attempts. Remaining attempt:", attempt)
-                    data.append(guess)
-        elif guess in data:
+                    data.append(tebak)
+        elif tebak in data:
             print("You already guess this letter!")
             print("You still have your attempts. Remaining attempt:", attempt)
             continue
@@ -30,12 +30,12 @@ def hangman(secret_word):
             print("This letter is not in the secret word.")
             attempt-=1
             print("You used 1 attempts. Remaining attempt:", attempt)
-            data.append(guess)
+            data.append(tebak)
         
-    if guess_word!=list(secret_word) and attempt==0:
+    if kata!=list(secret_word) and attempt==0:
         print("Oh no! You ran out of attempts, you lose!!")
 
-    print(' '.join(guess_word))
+    print(' '.join(kata))
     print("Congratulations! You win!")
 
 hangman('lab')
